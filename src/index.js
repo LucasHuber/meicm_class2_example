9d84a1699131a946891104feed8b6f7a0f00c7ca
Lucas Huber <lucas.huber@omicronenergy.com>
2024-03-18 19:53:26 +0000

    Added some changes to test google

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const port = process.argv[2] || process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
-    res.send({ 'date': new Date(), 'text': faker.lorem.paragraph()});
+    res.send({ 'date': new Date(), 'test': "This is a test text", 'text': faker.lorem.paragraph()});
 });
 
 app.listen(port, () => console.log(`App started on ${port}!`));
